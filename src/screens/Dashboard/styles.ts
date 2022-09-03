dcda0b1c494774e134d68533befe81a3bcbc96e0
LucasGTorres <lucas.gregtorres@gmail.com>
2022-09-03 17:02:54 -0300

    aula 2 cap 2

---
+++ src/screens/Dashboard/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    flex: 1;
+    justifyContent: center;
+    alignItems: center;
+    backgroundColor: '#999;
+`;
+
+export const Title = styled.Text`
+    font-size: 24px;
+    font-weight: bold;
+    color: red;
+`;
