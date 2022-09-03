d8bee6b7ac7c39a1c7da66b3bae9ec680f6a29a1
tkzn <andre.yabiku@hotmail.com>
2022-09-03 17:44:53 -0300

    aula 2 cap 3

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -2,13 +2,13 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
     flex: 1;
-    justifyContent: center;
-    alignItems: center;
-    backgroundColor: '#999;
+    justify-content: center;
+    align-items: center;
+    background-color: ${({ theme }) => theme.colors.primary} ;
 `;
 
 export const Title = styled.Text`
     font-size: 24px;
     font-weight: bold;
-    color: red;
+    color: ${({theme}) => theme.colors.text};
 `;
