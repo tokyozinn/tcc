9dd35c673736acad03f001f3ab8d0a9530983b67
tkzn <andre.yabiku@hotmail.com>
2022-09-03 18:54:15 -0300

    aula 5 cap 2

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,13 +1,31 @@
 import React from "react";
 
-import { Container, Title } from "./styles";
+import {
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+
+} from "./styles";
 
 export function Dashboard() {
     return (
-            <Container>
-                <Title>
-                    Dashboard
-                </Title>
-            </Container>
+        <Container>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/52468585?v=4' }} />
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>Tokyozinn</UserName>
+                        </User>
+                    </UserInfo>
+                </UserWrapper>
+            </Header>
+        </Container>
     )
 }
